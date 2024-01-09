b02862cf661cf66e9c4e73192f30c28bfd64fb13
ahmed El-Atreby <ahmedsport@hotmail.com>
2024-01-09 15:29:28 +0000

    create db

--- add.php
+++ add.php
@@ -1,59 +1,81 @@
 <?php
 
+$email = $title = $ingredients = '';
+$errors = array('email' => '', 'title' => '', 'ingredients' => '');
 
-if (isset($_POST["submit"])) {
+if (isset($_POST['submit'])) {
 
-    // Check the email is not empty
-    if (empty($_POST["email"])) {
-        echo "An Email is required <br/>";
+    // check email
+    if (empty($_POST['email'])) {
+        $errors['email'] = 'An email is required';
     } else {
-        $email = $_POST["email"];
+        $email = $_POST['email'];
         if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-            echo " email must be a viled email address <br/>";
+            $errors['email'] = 'Email must be a valid email address';
         }
     }
 
-    // Check the title is not empty
-    if (empty($_POST["title"])) {
-        echo "An title is required <br/>";
+    // check title
+    if (empty($_POST['title'])) {
+        $errors['title'] = 'A title is required';
     } else {
-        echo htmlspecialchars($_POST["title"]);
+        $title = $_POST['title'];
+        if (!preg_match('/^[a-zA-Z\s]+$/', $title)) {
+            $errors['title'] = 'Title must be letters and spaces only';
+        }
     }
 
-    // Check the ingredients is not empty
-    if (empty($_POST["ingredients"])) {
-        echo "An ingredients is required <br/>";
+    // check ingredients
+    if (empty($_POST['ingredients'])) {
+        $errors['ingredients'] = 'At least one ingredient is required';
     } else {
-        echo htmlspecialchars($_POST["ingredients"]);
+        $ingredients = $_POST['ingredients'];
+        if (!preg_match('/^([a-zA-Z\s]+)(,\s*[a-zA-Z\s]*)*$/', $ingredients)) {
+            $errors['ingredients'] = 'Ingredients must be a comma separated list';
+        }
     }
-} // end of POST check
 
+    if (array_filter($errors)) {
+        // echo 'errors in form';
+    } else {
+        // echo 'form is valid';
+        header('Location: index.php');
+    }
+
+
+} // end POST check
 
 ?>
 
 <!DOCTYPE html>
 <html>
 
-<?php include "templates/header.php"; ?>
+<?php include('templates/header.php'); ?>
+
 <section class="container grey-text">
-    <h4 class="center">Add A Pizza</h4>
-    <form action="add.php" method="POST" class="white">
-        <label>Your Email:</label>
-        <input type="text" name="email">
-
-        <label>Pizza Title:</label>
-        <input type="text" name="title">
-
-        <label>Ingredients (comma separated):</label>
-        <input type="text" name="ingredients">
+    <h4 class="center">Add a Pizza</h4>
+    <form class="white" action="add.php" method="POST">
+        <label>Your Email</label>
+        <input type="text" name="email" value="<?php echo htmlspecialchars($email) ?>">
+        <div class="red-text">
+            <?php echo $errors['email']; ?>
+        </div>
+        <label>Pizza Title</label>
+        <input type="text" name="title" value="<?php echo htmlspecialchars($title) ?>">
+        <div class="red-text">
+            <?php echo $errors['title']; ?>
+        </div>
+        <label>Ingredients (comma separated)</label>
+        <input type="text" name="ingredients" value="<?php echo htmlspecialchars($ingredients) ?>">
+        <div class="red-text">
+            <?php echo $errors['ingredients']; ?>
+        </div>
         <div class="center">
-            <input type="submit" name="submit" value="submit" class="btn brand z-depth-0">
+            <input type="submit" name="submit" value="Submit" class="btn brand z-depth-0">
         </div>
     </form>
 </section>
 
-<?php include "templates/footer.php"; ?>
-
-
+<?php include('templates/footer.php'); ?>
 
 </html>
--- index.php
+++ index.php
@@ -1,14 +1,32 @@
 <?php
 
+// connect to db
+$conn = mysqli_connect("localhost", "ahmed", "test123", "pizza");
 
+// check connection
+
+if (!$conn) {
+    echo "Connection error: " . mysqli_connect_error();
+}
+
+// write query for all pizza
+$sql = 'SELECT title, ingredients, id FROM pizzas';
+
+// make query and get result
+$result = mysqli_query($conn, $sql);
+
+// fetch the resulting rows as an array
+$pizzas = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+print_r($pizzas);
 
 ?>
 
 <!DOCTYPE html>
 <html>
 
-<?php include "templates/header.php";?>
-<?php include "templates/footer.php";?>
+<?php include "templates/header.php"; ?>
+<?php include "templates/footer.php"; ?>
 
 
 
