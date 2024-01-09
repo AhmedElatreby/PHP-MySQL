4d2a1d8f7820cf1df3e6a8e341ee8a154d4eadca
ahmed El-Atreby <ahmedsport@hotmail.com>
2024-01-09 15:53:51 +0000

    rending data

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ if (!$conn) {
 }
 
 // write query for all pizza
-$sql = 'SELECT title, ingredients, id FROM pizzas';
+$sql = 'SELECT title, ingredients, id FROM pizzas ORDER BY created_at';
 
 // make query and get result
 $result = mysqli_query($conn, $sql);
@@ -18,7 +18,12 @@ $result = mysqli_query($conn, $sql);
 // fetch the resulting rows as an array
 $pizzas = mysqli_fetch_all($result, MYSQLI_ASSOC);
 
-print_r($pizzas);
+// free result from memory
+mysqli_free_result($result);
+
+// close connection
+mysqli_close($conn);
+
 
 ?>
 
@@ -26,6 +31,31 @@ print_r($pizzas);
 <html>
 
 <?php include "templates/header.php"; ?>
+<h4 class="center gery-text">Pizzas!</h4>
+<div class="container">
+    <div class="row">
+        <?php foreach ($pizzas as $pizza) { ?>
+
+            <div class="col s6 md3">
+                <div class="card z-depth-0">
+                    <div class="card-content center">
+                        <h6>
+                            <?php echo htmlspecialchars($pizza["title"]); ?>
+                        </h6>
+
+                        <div>
+                            <?php echo htmlspecialchars($pizza["ingredients"]); ?>
+                        </div>
+                    </div>
+                    <div class="card-action right-align">
+                        <a class="brand-text" href=" #">more info</a>
+                    </div>
+                </div>
+            </div>
+
+        <?php } ?>
+    </div>
+</div>
 <?php include "templates/footer.php"; ?>
 
 
