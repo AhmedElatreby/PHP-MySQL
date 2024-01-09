49e7542a73fa098370090e13f8be7648bfc9cc1d
ahmed El-Atreby <ahmedsport@hotmail.com>
2024-01-09 21:42:21 +0000

    session

--- details.php
+++ details.php
@@ -3,23 +3,14 @@
 include("config/connect_db.php");
 if(isset($_POST["delete"])){
     $id_to_delete = mysqli_real_escape_string($conn, $_POST['id_to_delete']);
-
     $sql = "DELETE FROM pizzas WHERE id = $id_to_delete";
-
     if(mysqli_query($conn, $sql)){
         // success
         header("Location: index.php");
-
     } else {
         // failure
         echo "query error" . mysqli_error($conn);
-
-    }
-
-   
-
-
-}
+    }}
 
 // check GET request id param
 if (isset($_GET["id"])) {
--- sandbox.php
+++ sandbox.php
@@ -2,10 +2,12 @@
 
 	if(isset($_POST['submit'])){
 
+		//cookie for gender
+		setcookie('gender', $_POST['gender'], time() + 86400);
+
 		session_start();
 
 		$_SESSION['name'] = $_POST['name'];
-        // echo $_SESSION["name"];
 
 		header('Location: index.php');
 	}
@@ -15,12 +17,16 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>php pizza</title>
+	<title>php tuts</title>
 </head>
 <body>
 
 	<form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="POST">
 		<input type="text" name="name">
+		<select name="gender">
+			<option value="male">male</option>
+			<option value="female">female</option>
+		</select>
 		<input type="submit" name="submit" value="submit">
 	</form>
 
